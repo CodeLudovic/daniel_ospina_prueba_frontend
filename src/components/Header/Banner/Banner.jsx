66070f7da0e7b065412b91bc36bce24a06b173f0
Daniel Ospina Ramirez <danielospinar@gmail.com>
2024-02-14 00:35:17 -0500

    Fixed Menu DropDown

--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -1,23 +1,30 @@
-// prettier-ignore
+/* eslint-disable */
 import styles from "./Banner.module.css";
 export const Banner = () => {
+	const actionButton = () => {
+		console.log("Go There");
+	};
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.subContainer}>
-				<div>
-					<h1 className={styles.titleBanner}>Lorem ipsum</h1>
+				<h1 className={styles.titleBanner}>Lorem ipsum</h1>
+				<h2 className={styles.subTitleBanner}>Neque porro quisquam</h2>
+				<div className={styles.paragraphs}>
+					<h5 className={styles.paragraph}>
+						"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+						consectetur, adipisci velit..." <br />
+						"There is no one who loves pain itself, who seeks after it and
+						wantsto have it, simply because it is pain..."
+					</h5>
 				</div>
-				<div>
-					<h2 className={styles.subTitleBanner}>Neque porro quisquam</h2>
-				</div>
-				<div className={styles.paragraph}>
-					"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
-					consectetur, adipisci velit..."
-				</div>
-				<div className={styles.paragraph}>
-					"There is no one who loves pain itself, who seeks after it and wants
-					to have it, simply because it is pain..."
-				</div>
+				<button className={styles.buttonBanner} onClick={() => actionButton()}>
+					<p className={styles.textButton}>Go there...</p>
+					<svg width="24px" height="24px" viewBox="0 0 24 24">
+						<rect width="24" height="24" fill="#0C1726" rx="12" ry="12" />
+						<polygon points="8,6 18,12 8,18" fill="white" />
+					</svg>{" "}
+				</button>
 			</div>
 		</div>
 	);
--- src/components/Header/NavBar/NavBar.jsx
+++ src/components/Header/NavBar/NavBar.jsx
@@ -1,18 +1,84 @@
+/* eslint-disable */
+import { useState } from "react";
 import { Animation } from "../Animation/Animation";
 import { Banner } from "../Banner/Banner";
 import styles from "./NavBar.module.css";
 export const NavBar = () => {
+	const [openedMenu, setOpenedMenu] = useState(false);
+	const [openedSubMenu, setOpenedSubMenu] = useState(false);
+
+	const handleMenuClick = () => {
+		setOpenedMenu(!openedMenu);
+		if (openedMenu) {
+			setOpenedSubMenu(false); // Cerrar submenú si se cierra el menú principal
+		}
+	};
+
+	const handleSubMenuClick = (e) => {
+		e.stopPropagation(); // Evitar que el evento se propague y cierre el menú principal
+		setOpenedSubMenu(!openedSubMenu);
+	};
+
+	const handleOutMenu = () => {
+		setOpenedMenu(false);
+		setOpenedSubMenu(false);
+	};
+
 	return (
 		<header className={styles.navBarContainer}>
-			<div className={styles.bannerNavContainer}>
+			<nav className={styles.bannerNavContainer}>
 				<ul className={styles.menuContainer}>
 					<li>Home</li>
-					<li>Pages</li>
+					<li
+						className={styles.submenu}
+						style={
+							openedMenu ? { fontWeight: "bold" } : { fontWeight: "normal" }
+						}
+						onMouseEnter={() => setOpenedMenu(true)}
+						//onMouseLeave={handleOutMenu}
+						onClick={handleMenuClick}>
+						Pages
+						<ul
+							className={styles.submenuContainer}
+							style={openedMenu ? { position: "fixed" } : { display: "none" }}>
+							<li className={styles.menuItem}>Page 1</li>
+							<li className={styles.menuItem}>Page 2</li>
+							<li
+								onClick={handleSubMenuClick}
+								style={
+									openedSubMenu
+										? { fontWeight: "bold" }
+										: { fontWeight: "normal" }
+								}>
+								<p className={styles.p3}>
+									Page 3{" "}
+									{!openedSubMenu ? (
+										<svg width="10" height="10" viewBox="0 0 10 10">
+											<polygon points="0,0 10,5 0,10" fill="white" />
+										</svg>
+									) : (
+										<svg width="10" height="10" viewBox="0 0 10 10">
+											<polygon points="0,0 5,10 10,0" fill="white" />
+										</svg>
+									)}
+								</p>
+								<ul
+									className={
+										openedSubMenu ? styles.contSub : styles.contSubHidden
+									}>
+									<li className={styles.submenuItem}>Item 1</li>
+									<li className={styles.submenuItem}>Item 1</li>
+									<li className={styles.submenuItem}>Item 1</li>
+								</ul>
+							</li>
+						</ul>
+					</li>
 					<li>About</li>
 					<li>Contact</li>
 				</ul>
 				<Banner />
-			</div>
+				<div style={{ marginTop: openedMenu ? "80px" : "0" }}></div>
+			</nav>
 			<div>
 				<Animation />
 			</div>
